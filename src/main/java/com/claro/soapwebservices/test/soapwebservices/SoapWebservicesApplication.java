2b54521f10957c7fcf7d86881b1e30e3bdc58cc3
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-10-08 21:48:30 -0300

    SOAP Web Services com Spring Boot

---
+++ src/main/java/com/claro/soapwebservices/test/soapwebservices/SoapWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.claro.soapwebservices.test.soapwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/claro/soapwebservices/test/soapwebservices/SoapWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claro.soapwebservices.test.soapwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
